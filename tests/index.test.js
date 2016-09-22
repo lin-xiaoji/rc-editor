752616d8e67cdecb53c66877e2d20478fd9a64ca
linxiaoji <406611515@qq.com>
2016-09-22 20:14:39 +0800

    update import html

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,10 +10,30 @@ import RcEditor from '../src/index'
 
 
 describe('rc-editor', function () {
+
     let editor;
     const container = document.createElement('div');
     document.body.appendChild(container);
 
+    function selectText(element) {
+        var text = element;
+        if (document.body.createTextRange) {
+            var range = document.body.createTextRange();
+            range.moveToElementText(text);
+            range.select();
+        } else if (window.getSelection) {
+            var selection = window.getSelection();
+            var range = document.createRange();
+            range.selectNodeContents(text);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            /*if(selection.setBaseAndExtent){
+             selection.setBaseAndExtent(text, 0, text, 1);
+             }*/
+        } else {
+            console.log('can not select');
+        }
+    }
     function renderWithProps(props) {
         return editor = render(
             <RcEditor
@@ -34,6 +54,9 @@ describe('rc-editor', function () {
     });
 
     it('bold works',()=> {
+        const content = TestUtils.scryRenderedDOMComponentsWithClass(editor,'public-DraftEditor-content')[0];
+        selectText(content);
+
         const bold = TestUtils.scryRenderedDOMComponentsWithClass(editor,'rc-editor-toolbar-item')[0];
         Simulate.click(bold);
 
